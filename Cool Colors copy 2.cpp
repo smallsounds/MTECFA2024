2aa6af1192549d825876187b78dfbe132e7f5889
smallsounds <157655032+smallsounds@users.noreply.github.com>
2024-09-26 16:22:27 -0400

    meow

---
+++ Cool Colors copy 2.cpp
@@ -0,0 +1,110 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofSetBackgroundAuto(false);
+    ofSetFrameRate(60);
+    ofEnableAlphaBlending();
+
+// We still want to draw on a black background, so we need to draw
+// the background before we do anything with the brush
+    ofBackground(0);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+if (ofGetMousePressed(OF_MOUSE_BUTTON_LEFT)) {  // If the left mouse button is pressed...
+    ofSetRectMode(OF_RECTMODE_CENTER);
+    float randomColor = ofRandom(50, 255);
+    float randomColor1 = ofRandom(50, 100);
+    float randomColor2 = ofRandom(50, 50);
+    int numRects = 10;
+    int maxRadius = 40;  // Increase for a wider brush
+    int radiusStepSize = 3;  // Decrease for more circles (i.e. a more opaque brush)
+    int alpha = 10;  // Increase for a more opaque brush
+    int maxOffsetDistance = 75;  // Increase for a larger spread of circles
+// draw smaller and smaller circles and layering (increasing) opaqueness
+    for (int radius=maxRadius; radius>0; radius-=radiusStepSize) {
+        float angle = ofRandom(ofDegToRad(360.0));
+        float distance = ofRandom(maxOffsetDistance);
+        float xOffset = cos(angle) * distance;
+        float yOffset = sin(angle) * distance;
+        ofSetColor(randomColor,randomColor1,randomColor2, alpha);
+        ofDrawCircle(ofGetMouseX()+xOffset, ofGetMouseY()+yOffset, radius);
+}
+    }
+
+    if (ofGetMousePressed(OF_MOUSE_BUTTON_RIGHT)) {  // If the right mouse button is pressed...
+    ofBackground(0);  // Draw a black background over the screen
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::exit(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseScrolled(int x, int y, float scrollX, float scrollY){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
